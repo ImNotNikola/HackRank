5ee1a1792881e82a487ee022f820055f6be93f64
Nikola <48845744+ImNotNikola@users.noreply.github.com>
2020-02-16 16:57:09 -0500

    Create Day2.py

---
+++ Python/30 Days of Code/Day2.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    tax_cost = meal_cost * tax_percent/100
+    tip_cost = meal_cost * tip_percent/100
+    total_cost = meal_cost + tip_percent + tax_percent
+    print(meal_cost + tax_cost + tip_cost)
